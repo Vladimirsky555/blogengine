407dc900c594e58d8906e89b10d207b2cb12cdcb
Vladimirsky <vladimir.bervin@gmail.com>
2020-07-02 10:08:33 +0300

    Update and Delete tags and posts

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.shortcuts import reverse
 
+from django.utils.text import slugify
+from time import time
+
+def get_slug(s):
+    new_slug = slugify(s, allow_unicode = True)
+    return new_slug + '-' + str(int(time()))
+
 class Post(models.Model):
     title = models.CharField(max_length = 150, db_index = True) #Для быстрого поиска
-    slug = models.SlugField(max_length = 150, unique = True) #unique = True автоматом индексируются
+    #slug = models.SlugField(max_length = 150, unique = True) #unique = True автоматом индексируются
+    slug = models.SlugField(max_length=150, blank = True, unique=True)
     content = models.TextField(blank = True, db_index = True) #Поле может быть пустым
     # blank = True - не у всех постов могут быть теги
     # related_name = 'posts' - свойство появится у экземпляров класса Tag
@@ -11,11 +19,24 @@ class Post(models.Model):
     tags = models.ManyToManyField('Tag', blank = True, related_name = 'posts')
     date_pub = models.DateTimeField(auto_now_add = True) #auto_now_add = True в момент сохранения в базе данных
 
+    #Переопределяем метод save(), чтобы добавлять slug автоматически при создании поста
+    def save(self, *args, **kwargs):
+        if not self.id: #Если пост не сохранён в базу, у него нет id
+            self.slug = get_slug(self.title)
+            super().save(*args, **kwargs) #передаю в суперкласс Model позиционные и именованные аргументы
+
+
     #Возвращает ссылку на конкретный объект, экземпляр класса Post
     #reverse и url одно и то же. Просто reverse используется в .py-файлах, url - используется
     #в шаблонах
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('post_delete_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
@@ -30,3 +51,9 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('tag_delete_url', kwargs={'slug': self.slug})
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.shortcuts import redirect
 from django.views.generic import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 from .forms import TagForm, PostForm
 
 
@@ -16,20 +17,6 @@ def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context = {'tags': tags})
 
-class PostCreate(View):
-    def get(self, request):
-        form = PostForm()
-        return render(request, 'blog/post_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = PostForm(request.POST)
-
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        return render(request, 'blog/post_create.html', context={'form': bound_form})
-
-
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
@@ -38,18 +25,31 @@ class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create.html'
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update.html'
 
-    def post(self, request):
-        bound_form = TagForm(request.POST)
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update.html'
 
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
 
